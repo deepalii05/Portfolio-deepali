dd7ae4c54cb4f6faa98e31176984b96a3d1dc08f
Deepali Thakur <77734636+deepalii05@users.noreply.github.com>
2023-04-01 10:17:14 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -80,7 +80,7 @@ function Header() {
 
           <Button
             onClick={() => {
-              window.open("https://drive.google.com/file/d/1U7HAQq40Y6wk6gtwOIQEvtNfn_oepC3q/view?usp=sharing");
+              window.open("https://drive.google.com/file/d/15CnQ8VG4Ncp8UspDgH_GuDQYo1c7oqnG/view?usp=sharing");
             }}
             className='resumebtn'><span>Resume</span>
           </Button>
